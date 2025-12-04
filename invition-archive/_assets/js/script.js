e161ea273f69e3f853e059a8a478ad1fbcef531a
01emastaff <matsufuji@emanet.jp>
2025-12-04 09:59:21 +0900

    # 002:feature: 機能内容記載

---
+++ invition-archive/_assets/js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+	
+const headerBg=$('.header');
+const headerBtn=$('.header li a');
+const headerHei= headerBg.outerHeight(); 
+
+$(window).on('scroll',()=>{
+	if($(this).scrollTop()>=100){
+	headerBg.addClass('bgWhitegrad');
+	}else{
+	headerBg.removeClass('bgWhitegrad');
+	}
+	
+});
+
+$(headerBtn).on('click',()=>{
+	//$(mainContent).scrollTop(200);
+	//const aniSpeed=500;
+	//const nextTo=0;
+	//const href=$(this).attr('href');
+	//const target=$(href=='#'||href==''?'html':href);
+	//const posi=target.offset().top+100;
+	//console.log("hello");
+})
+});
